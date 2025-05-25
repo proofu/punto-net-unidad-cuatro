76b1f4baf58b20216c616a527fd855e42f958dd6
alfred <profumieri.a@gmail.com>
2025-05-25 17:23:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad4OrtizProfumieriUnzaga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
